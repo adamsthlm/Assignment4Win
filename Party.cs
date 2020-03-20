9790ffbf77233491a718be68a069b28627d8621b
Carl-Adam Berglund <adam@carl-adam.tech>
2020-03-20 21:00:38 +0100

    First commit

---
+++ Party.cs
@@ -0,0 +1,29 @@
+﻿/*
+Denna fil har skapats som en del av kursen [C# I] på Malmö Universitet 2020
+Namn: Carl-Adam Berglund
+e-mail: ak7764@mau.se
+*/
+
+class Party
+{
+    private double costPerCapita;
+    private string[] guestList;
+
+    //constructor
+    public Party(int maxNumOfGuests)
+    {
+        guestList = new string[maxNumOfGuests];
+    }
+
+    public double CostPerCapita
+    {
+        get { return costPerCapita; }
+        set
+        {
+            if (value >= 0)
+            {
+                costPerCapita = value;
+            }
+        }
+    }
+}
